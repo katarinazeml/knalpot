ec112ea6e577646e766c92e274d94f4026e9b839
kazeml <kazeml@taltech.ee>
2023-02-17 11:15:37 +0200

    lol

---
+++ server/theServer/src/main/java/server/TheServer.java
@@ -0,0 +1,11 @@
+package server;
+
+public class TheServer {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TheServer().getGreeting());
+    }
+}
