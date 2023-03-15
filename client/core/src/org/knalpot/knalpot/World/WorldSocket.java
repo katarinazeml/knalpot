0bc3560983edcdfc2a53302b1e64af6bdee27d42
kazeml <kazeml@taltech.ee>
2023-03-15 19:17:46 +0200

    I did something

---
+++ client/core/src/org/knalpot/knalpot/World/WorldSocket.java
@@ -0,0 +1,32 @@
+package org.knalpot.knalpot.World;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.io.*;
+import java.util.*;
+
+public class WorldSocket {
+    public static void main(String[] args) {
+        try {
+            try (Socket s = new Socket("localhost", 8080)) {
+                InputStream inStream = s.getInputStream();
+                Scanner in = new Scanner(inStream);
+
+                while (in.hasNext()) {
+                    if (in.hasNextInt()) {
+                        int intValue = in.nextInt();
+                        System.out.println("Received an int value: " + intValue);
+                    } else if (in.hasNextFloat()) {
+                        float floatValue = in.nextFloat();
+                        System.out.println("Received a float value: " + floatValue);
+                    } else {
+                        String line = in.nextLine();
+                        System.out.println("Received a message:" + line);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
