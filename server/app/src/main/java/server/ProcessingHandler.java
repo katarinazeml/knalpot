2cea90d6b1749fd159c06e124b06b6a6dc0c4800
kazeml <kazeml@taltech.ee>
2023-03-15 14:17:39 +0200

    server chronicles

--- server/app/src/main/java/server/ProcessingHandler.java
+++ server/app/src/main/java/server/ProcessingHandler.java
@@ -1,26 +1,48 @@
 package server;
 
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
-// Create a temporary ByteBuf and append to it all inbound bytes until we get the required amount of bytes.
 public class ProcessingHandler
         extends ChannelInboundHandlerAdapter {
+    private static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            channel.writeAndFlush("Server " + incoming.remoteAddress() + " has joined");
+        }
+        channels.add(ctx.channel());
+    }
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            channel.writeAndFlush("Server " + incoming.remoteAddress() + " has disconnected");
+        }
+        channels.remove(ctx.channel());
+    }
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg)
             throws Exception {
 
         RequestData requestData = (RequestData) msg;
         ResponseData responseData = new ResponseData();
-        // Let's assume that the server receives the request and returns the intValue multiplied by 2.
-        responseData.setIntValue(requestData.getIntValue() * 2);
-//        responseData.setStringValue(requestData.getStringValue().toUpperCase());
+        responseData.setIntValue(requestData.getIntValue());
+        responseData.setStringValue(requestData.getStringValue());
         responseData.setFloatValue(requestData.getFloatValue());
-        System.out.println("sent back response from server");
-        ChannelFuture future = ctx.writeAndFlush(responseData);
+        // System.out.println("sent back response from server");
+        // ChannelFuture future = ctx.writeAndFlush(responseData);
         //future.addListener(ChannelFutureListener.CLOSE);
         //ctx.write(requestData);
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            if (channel != incoming) {
+                channel.writeAndFlush(incoming.remoteAddress() + " coordinates: " + responseData);
+            }
+        }
     }
 }
--- server/app/src/main/java/server/RequestDecoder.java
+++ server/app/src/main/java/server/RequestDecoder.java
@@ -19,15 +19,15 @@ public class RequestDecoder extends ReplayingDecoder<RequestData> {
         RequestData data = new RequestData();
         System.out.println("data on server");
 //        data.setIntValue(in.readInt());
-//        int strLen = in.readInt();
+        int strLen = in.readInt();
 //        System.out.println(strLen);
-//        // CharSequence str = in.readCharSequence(strLen, charset);
+        CharSequence str = in.readCharSequence(strLen, charset);
 //        System.out.println("might be a position");
 //        System.out.println(str);
-//        data.setStringValue(str.toString());
+        data.setStringValue(str.toString());
 //        System.out.println(data.getStringValue());
         data.setFloatValue(in.readFloat());
-        System.out.println(data.getFloatValue());
+//        System.out.println(data.getFloatValue());
         out.add(data);
     }
 }
