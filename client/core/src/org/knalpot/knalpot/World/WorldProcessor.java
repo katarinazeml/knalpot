2fa405fce4791ac1d4362bbb796eb3f7391fb902
kazeml <kazeml@taltech.ee>
2023-03-19 11:10:08 +0200

    I did something

--- client/core/src/org/knalpot/knalpot/World/WorldProcessor.java
+++ client/core/src/org/knalpot/knalpot/World/WorldProcessor.java
@@ -4,10 +4,7 @@ import org.knalpot.knalpot.Interactive.CollisionBlock;
 import org.knalpot.knalpot.Player.Player;
 import org.knalpot.knalpot.Player.PlayerProcessor;
 
-import java.io.*;
 import java.net.Socket;
-import java.io.InputStream;
-
 
 public class WorldProcessor {
 	private World world;
@@ -17,8 +14,6 @@ public class WorldProcessor {
 	private CollisionBlock block;
 
 	private Socket socket;
-	private DataOutputStream dOut;
-	private DataInputStream dIn;
 
 
 	public WorldProcessor(World world) {
@@ -26,22 +21,9 @@ public class WorldProcessor {
 		player = world.getPlayer();
 		block = world.getCollisionBlocks();
 		playerProcessor = new PlayerProcessor(world);
-		try {
-			socket = new Socket("localhost", 8080);
-			dOut = new DataOutputStream(socket.getOutputStream());
-			dIn = new DataInputStream(socket.getInputStream());
-		} catch (IOException e) {
-			System.out.println(e);
-		}
 	}
 
 	public void update(float dt) {
 		playerProcessor.update(dt);
-		try {
-			dOut.writeFloat(player.getPosition().x);
-			dOut.flush();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
 	}
 }
