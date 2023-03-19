c08d6d3a1c6018cf82528568a6e672b8f20d0860
kazeml <kazeml@taltech.ee>
2023-03-19 11:14:55 +0200

    server chronicles

--- server/app/src/main/java/ServerFoundation.java
+++ server/app/src/main/java/ServerFoundation.java
@@ -1,29 +1,62 @@
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import ServerPlayer.Game;
+import ServerPlayer.ServerPlayer;
 
 public class ServerFoundation {
-    public static ServerFoundation instance;
     private Server server;
-    public static void main(String[] args) {
-        ServerFoundation.instance = new ServerFoundation();
-    }
+    private final int port = 8080;
+    private Game game;
 
     public ServerFoundation() {
-        this.server = new Server(1_000_000, 1_000_000);
-        this.bindServer(8080, 8081);
+        server = new Server();
+        game = new Game();
+        registerPackets();
+        server.start();
+        try {
+            server.bind(port);
+            System.out.print("Server started on port: " + port +
+                    " | start time: " + LocalDateTime.now());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        server.addListener(new Listener() {
+            public void received(Connection connection, Object object) {
+                System.out.println("New connection! Connected ID: "
+                        + connection.getID() + " date: " + LocalDateTime.now());
+                if (object instanceof ServerPlayer) {
+                    ServerPlayer packet = (ServerPlayer) object;
+                    // update player coordinates
+                    updatePlayerCoordinates(connection.getID(), packet.getX(), packet.getY());
+                    System.out.println("X coordinate: " + packet.getX() + "Y coordinate: " + packet.getY());
+                    broadcastPacket(packet);
+                }
+            }
+        });
     }
 
-    public void bindServer(final int tcpPort, final int udpPort) {
-        this.server.start();
-        try {
-            this.server.bind(tcpPort, udpPort);
-            System.out.print("Server started | tcp port: " + tcpPort + " | udp port: " + udpPort
-                    + " | time: " + LocalDateTime.now());
-        } catch (IOException e) {
-            System.out.println("Connection to the server failed!\n Exception: " + e);
-            System.exit(0);
+    private void registerPackets() {
+        server.getKryo().register(ServerPlayer.class);
+        // add more packet classes here if needed
+    }
+
+    private void broadcastPacket(Object object) {
+        for (Connection connection : server.getConnections()) {
+            connection.sendTCP(object);
         }
+    }
+    private void updatePlayerCoordinates(int connectionID, float x, float y) {
+        // find the player with the given connection ID and update their coordinates
+        ServerPlayer player = game.getPlayer(connectionID);
+        player.setX(x);
+        player.setY(y);
+    }
+
+    public static void main(String[] args) {
+        new ServerFoundation();
     }
 }
