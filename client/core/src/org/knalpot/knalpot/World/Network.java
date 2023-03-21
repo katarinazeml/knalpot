c5bae0f63436e025a46777eef80005227867c1e7
kazeml <kazeml@taltech.ee>
2023-03-21 13:36:09 +0200

    Fixing missing Network class

---
+++ client/core/src/org/knalpot/knalpot/World/Network.java
@@ -0,0 +1,63 @@
+package org.knalpot.knalpot.world;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import org.knalpot.knalpot.networking.*;
+
+public class Network extends Listener {
+    String ip = "localhost";
+    public static int port = 8082;
+    private Client client = new Client();
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void connect() {
+        register(client);
+        client.addListener(this);
+
+        client.start();
+        System.out.println("Client is started");
+        try {
+            System.out.println("Trying to connect to the server.");
+            client.connect(5000, InetAddress.getByName(ip), port, port);
+            System.out.println("Connected to the server.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void register(Client client) {
+        client.getKryo().register(PacketUpdateX.class);
+        client.getKryo().register(PacketUpdateY.class);
+        client.getKryo().register(PacketAddPlayer.class);
+        client.getKryo().register(PacketRemovePlayer.class);
+    }
+
+    public void received(Connection c, Object o){
+        if(o instanceof PacketAddPlayer){
+            PacketAddPlayer packet = (PacketAddPlayer) o;
+            MPPlayer newPlayer = new MPPlayer();
+            newPlayer.id = packet.id;
+            ClientProgram.players.put(packet.id, newPlayer);
+
+        }else if(o instanceof PacketRemovePlayer){
+            PacketRemovePlayer packet = (PacketRemovePlayer) o;
+            ClientProgram.players.remove(packet.id);
+
+        }else if(o instanceof PacketUpdateX){
+            PacketUpdateX packet = (PacketUpdateX) o;
+            ClientProgram.players.get(packet.id).x = packet.x;
+
+        }else if(o instanceof PacketUpdateY){
+            PacketUpdateY packet = (PacketUpdateY) o;
+            ClientProgram.players.get(packet.id).y = packet.y;
+        }
+    }
+}
+
